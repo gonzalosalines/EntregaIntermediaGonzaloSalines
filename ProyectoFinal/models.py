996892aeb1a5c4dd7255087fc14fca120bcbb5d8
Gonzalo Salines <gonzalosalines@gmail.com>
2022-11-12 14:43:20 -0300

    App & model Creation

---
+++ ProyectoFinal/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Equipo(models.Model):
+
+    club=models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=45)
+
+    #def __str__(self):
+    #    return f"Curso: {self.nombre} - Camada: {self.camada}"
+
+
+class Jugador(models.Model):
+    nombre= models.CharField(max_length=35)
+    apellido= models.CharField(max_length=35)
+    edad= models.IntegerField()
+    puesto= models.CharField(max_length=30)
+
+class Entrenador(models.Model):
+    nombre= models.CharField(max_length=35)
+    apellido= models.CharField(max_length=35)
+    email= models.EmailField()
+    
