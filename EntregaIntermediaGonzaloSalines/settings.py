d875f4925124360492f1ab1bd686f6d63fffde10
Gonzalo Salines <gonzalosalines@gmail.com>
2022-11-12 15:24:41 -0300

    Settings.py DIR set up

--- EntregaIntermediaGonzaloSalines/settings.py
+++ EntregaIntermediaGonzaloSalines/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'EntregaIntermediaGonzaloSalines.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r"G:\Gonzalo\Repositorios\EntregaIntermediaGonzaloSalines\EntregaIntermediaGonzaloSalines\ProyectoFinal\templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- ProyectoFinal/views.py
+++ ProyectoFinal/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from ProyectoFinal.models import Curso, Profesor, Estudiante
+from ProyectoFinal.models import Equipo, Jugador, Entrenador
 #from ProyectoFinal.forms import ProfesorFormulario, EstudianteFormulario
 from django.shortcuts import render
+from django.template import loader # es realmente necesario?
 
 # Create your views here.
 
