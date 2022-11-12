035063686c9f8b298ce02b91c6d3e98144256eb0
Gonzalo Salines <gonzalosalines@gmail.com>
2022-11-12 15:17:31 -0300

    URLs & Views 1

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from ProyectoFinal.views import *
 
 urlpatterns = [
+    path("inicio/", inicio, name="coder-inicio"),
+    path("estudiantes/", jugadores, name="coder-jugadores"),
     
 ]
--- ProyectoFinal/views.py
+++ ProyectoFinal/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
+from ProyectoFinal.models import Curso, Profesor, Estudiante
+#from ProyectoFinal.forms import ProfesorFormulario, EstudianteFormulario
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+    return render(request, "ProyectoFinal/base.html")
+
+def jugadores(request):
+
+    return render(request, "ProyectoFinal/jugadores.html")
